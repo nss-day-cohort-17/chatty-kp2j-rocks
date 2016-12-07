a89c56733cbfc93185e357f535440471620156f7
Jufebrown <jufebrown@gmail.com>
2016-12-07 13:49:27 -0600

    added event listener for dark theme checkbox and a function to use it as a toggle

--- mainXHR.js
+++ mainXHR.js
@@ -1,3 +1,4 @@
+// variable for dark theme checkbox
 var darkTheme = document.getElementById("darkCheck");
 
 //creating new xhr for getting the json file
@@ -11,10 +12,26 @@ function chattyRoom (e) {
 var data = JSON.parse(e.target.responseText);
 console.log("data", data)
 }
+
  //keypress event
- document.getElementById("formControl").addEventListener("keypress", kbevt);
 function kbevt(event) {
     if(event.keyCode === 13) {
       console.log("you pressed return key")
     }
 }
+
+document.getElementById("formControl").addEventListener("keypress", kbevt);
+
+// function for darkTheme checkbox
+function darkToggle() {
+  if (darkTheme.checked) {
+    document.body.style.background = "darkgray";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+  }
+}
+
+// event listener for dark theme checkbox
+darkTheme.addEventListener("click", darkToggle);
