3c0459de9c5c060ebcdb356f9b02b63b9d074b37
wierje <wier.je@gmail.com>
2016-12-07 18:12:16 -0600

    added disable button function and html mods

--- mainXHR.js
+++ mainXHR.js
@@ -1,9 +1,21 @@
 // variable for clearbutton
 var clearButton = document.getElementById("clearButton");
-    // variable for dark theme checkbox
+// variable for dark theme checkbox
 var darkTheme = document.getElementById("darkCheck");
-    // variable for large text checkbox
+// variable for large text checkbox
 var makeLarge = document.getElementById('largeText');
+
+// to disable clear button and to call this and in all preceding functions
+function checkChatBox() {
+    if (message.innerHTML === "") {
+        console.log('if checkChatBox running');
+        clearButton.disabled = true;
+    } else {
+        console.log('else checkChatBox running', chatBox.innerHTML);
+        clearButton.disabled = false;
+    }
+}
+
 //creating new xhr for getting the json file
 var myRequest = new XMLHttpRequest();
 myRequest.addEventListener("load", chattyRoom);
@@ -26,18 +38,9 @@ function kbevt(event) {
     }
 }
 
+document.getElementById("textBar").focus();
 document.getElementById("textBar").addEventListener("keypress", kbevt);
 
-// function for darkTheme checkbox
-function darkToggle() {
-    if (darkTheme.checked) {
-        document.body.style.background = "darkgray";
-        document.body.style.color = "white";
-    } else {
-        document.body.style.background = "white";
-        document.body.style.color = "black";
-    }
-}
 
 // function to clear all messages
 function clearMessages() {
@@ -50,9 +53,18 @@ clearButton.addEventListener("click", clearMessages);
 
 // event listener for dark theme checkbox
 darkTheme.addEventListener("click", darkToggle);
+// function for darkTheme checkbox
+function darkToggle() {
+    if (darkTheme.checked) {
+        document.body.style.background = "darkgray";
+        document.body.style.color = "white";
+    } else {
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+    }
+}
 // event listener for large text checkbox
 makeLarge.addEventListener("click", largeToggle);
-
 // function for large text checkbox
 function largeToggle() {
     if (makeLarge.checked) {
@@ -63,23 +75,18 @@ function largeToggle() {
 }
 
 
-//function to create message a new message
+//function to create a new message
 
 var mainContent = "";
 
-
-
-
-
 function newMessage() {
-  var mainContent = document.getElementById("textBar").value;
-  console.log( document.getElementById("textBar").value)
-    mainContent = `<p>${mainContent}<button>Delete</button></p>`
-  document.getElementById("message").innerHTML +=mainContent;
+    var mainContent = document.getElementById("textBar").value;
+    console.log(document.getElementById("textBar").value);
+    mainContent = `<p>${mainContent}<button>Delete</button></p>`;
+    document.getElementById("message").innerHTML += mainContent;
+    document.getElementById("textBar").innerHTML = "";
 
 }
-
-
 
 //put the messages on the json file to messages area
 var mainMessage = "";
